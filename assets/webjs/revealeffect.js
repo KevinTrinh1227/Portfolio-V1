b0fc35fbbcca04f9e61e5ee298e38cb5dfb52335
KevinTrinh2021 <kevintrinh1227@gmail.com>
2022-10-17 02:12:02 -0500

    Update revealeffect.js

--- assets/webjs/revealeffect.js
+++ assets/webjs/revealeffect.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', reveal);
 
-function reveal(){
+function reveal() {
   var reveals = document.querySelectorAll('.reveal');
 
   for(var i = 0; i < reveals.length; i++){
@@ -9,11 +9,8 @@ function reveal(){
     var revealtop = reveals[i].getBoundingClientRect().top;
     var revealpoint = 150;
 
-    if(revealtop < windowheight - revealpoint){
+    if(revealtop < windowheight - revealpoint) {
       reveals[i].classList.add('active');
-    }
-    else{
-      reveals[i].classList.remove('active');
     }
   }
 }
