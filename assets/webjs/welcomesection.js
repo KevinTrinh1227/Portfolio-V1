d11f7ac03e584a1d274fbe74a2b7533c1b40e22c
Kevin Huy Trinh <kevintrinh1227@gmail.com>
2022-11-18 05:19:03 -0600

    Intro Section Script Update

--- assets/webjs/welcomesection.js
+++ assets/webjs/welcomesection.js
@@ -1,20 +1,32 @@
-let startText = "<span class='comment'>/* ----------- <br/>char siteAuthor[] = 'Kevin Huy Trinh'<br/>char currentClassification[] = 'Sophomore' <br/>char lastUpdated[] = ";
-let endText = "</br> --------- */</span></br></br>";
-result = startText.concat("'11/18/2022'", endText);
+const apiUrl = 'https://api.github.com/repos/gazijarin/GameCentre';
+let lastUpdated;
+async function getDate() {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    lastUpdated = data.updated_at;
 
-document.getElementsByTagName("p")[1].innerHTML = result;
+    console.log(lastUpdated);
 
-var str = document.getElementsByTagName("section")[0].innerHTML.toString();
-var i = 0;
-document.getElementsByTagName("section")[0].innerHTML = "";
+    let startText = "<span class='comment'>/* ----------- <br/>char siteAuthor[] = 'Kevin Huy Trinh'<br/>char currentClassification[] = 'Sophomore' <br/>char lastUpdated[] = '";
+    let endText = "'</br> --------- */</span></br></br>";
+    result = startText.concat(lastUpdated, endText);
 
-setTimeout(function() {
-    var se = setInterval(function() {
-        i++;
-        document.getElementsByTagName("section")[0].innerHTML = str.slice(0, i) + "|";
-        if (i == str.length) {
-            clearInterval(se);
-            document.getElementsByTagName("section")[0].innerHTML = str;
-        }
-    }, 10);
-});
+    document.getElementsByTagName("p")[1].innerHTML = result;
+
+    var str = document.getElementsByTagName("section")[0].innerHTML.toString();
+    var i = 0;
+    document.getElementsByTagName("section")[0].innerHTML = "";
+
+    setTimeout(function() {
+        var se = setInterval(function() {
+            i++;
+            document.getElementsByTagName("section")[0].innerHTML = str.slice(0, i) + "|";
+            if (i == str.length) {
+                clearInterval(se);
+                document.getElementsByTagName("section")[0].innerHTML = str;
+            }
+        }, 10);
+    });
+}
+
+getDate();
