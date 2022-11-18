e588a82ad1fdfacf5002852fa1ab2761b0af97b2
Kevin Huy Trinh <kevintrinh1227@gmail.com>
2022-11-18 03:40:06 -0600

    Update welcomesection.js

--- assets/webjs/welcomesection.js
+++ assets/webjs/welcomesection.js
@@ -1,13 +1,8 @@
 let startText = "<span class='comment'>/* ----------- <br/>char siteAuthor[] = 'Kevin Huy Trinh'<br/>char currentClassification[] = 'Sophomore' <br/>char lastUpdated[] = ";
 let endText = "</br> --------- */</span></br></br>";
 result = startText.concat("'11/18/2022'", endText);
-var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time;
-let currentClassification = "Sophomore"
 
-document.getElementsByTagName("p")[1].innerHTML = dateTime;
+document.getElementsByTagName("p")[1].innerHTML = result;
 
 var str = document.getElementsByTagName("section")[0].innerHTML.toString();
 var i = 0;
