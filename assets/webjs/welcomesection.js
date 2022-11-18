e4fba48161302b2ed542179a8393fff392f5f055
Kevin Huy Trinh <kevintrinh1227@gmail.com>
2022-11-17 22:27:24 -0600

    Updates

--- assets/webjs/welcomesection.js
+++ assets/webjs/welcomesection.js
@@ -1,5 +1,6 @@
-document.getElementsByTagName("p")[1].innerHTML =
-    "<span class='comment'>/* ----------- <br/>char siteAuthor[] = 'Kevin Huy Trinh'<br/>char currentClassification[] = 'Sophomore' <br/>char lastUpdated[] = '11/14/2022'</br> --------- */</span></br></br>";
+let text = "<span class='comment'>/* ----------- <br/>char siteAuthor[] = 'Kevin Huy Trinh'<br/>char currentClassification[] = 'Sophomore' <br/>char lastUpdated[] = '11/17/2022'</br> --------- */</span></br></br>";
+
+document.getElementsByTagName("p")[1].innerHTML = text;
 
 var str = document.getElementsByTagName("section")[0].innerHTML.toString();
 var i = 0;
