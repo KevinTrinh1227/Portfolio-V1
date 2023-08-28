3f5cc3c3921ca62e2c962ab5b22e00aa6736b0b9
Kevin Huy Trinh <kevintrinh1227@gmail.com>
2023-08-28 04:58:44 -0500

    update status

--- assets/webjs/welcomesection.js
+++ assets/webjs/welcomesection.js
@@ -21,7 +21,7 @@ async function getDate() {
 
   // concats the strings and displays them
   let startText =
-    "<span class='comment'>/* ----------- <br/>char siteAuthor[] = 'Kevin Huy Trinh'<br/>char currentClassification[] = 'Junior' <br/>char lastUpdated[] = '";
+    "<span class='comment'>/* ----------- <br/>char siteAuthor[] = 'Kevin Huy Trinh'<br/>char currentClassification[] = 'Sophomore' <br/>char lastUpdated[] = '";
   let endText = "'</br> --------- */</span></br></br>";
   result = startText.concat(updateDate, endText);
 
